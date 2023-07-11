739ba44c91b76b3794d908cea6efb038773a7d98
Your Name <you@example.com>
2023-07-11 17:03:39 +0530

    updated to lil descriptive-part1

---
+++ 04-dynamic variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#In scripting languages, dynamic values typically refer to data that can change or be modified during the execution of a program.
+#These values are not fixed or predetermined but can be assigned, updated, or manipulated based on various conditions or actions within the script.
+
+Todays-date = $(date+%F)
