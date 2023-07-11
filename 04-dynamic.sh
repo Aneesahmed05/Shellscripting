bd95ff3d593b08f35ff82d024207c54ba6036770
Your Name <you@example.com>
2023-07-11 18:17:20 +0530

    updated to lil descriptive-part1

--- 04-dynamic.sh
+++ 04-dynamic.sh
@@ -4,6 +4,3 @@
 #These values are not fixed or predetermined but can be assigned, updated, or manipulated based on various conditions or actions within the script.
 
 TODAYS_DATE=$(date +%F)                    
-NO_OF_SESSIONS=$(who | wc -l)                                                          # Always enclose expresssions in paranthesis
-echo -e "Good Morning and todays date is \e[32m $TODAYS_DATE \e[0m"
-echo -e "Number of opened sessions : \e[31m $NO_OF_SESSIONS \e[0m"
