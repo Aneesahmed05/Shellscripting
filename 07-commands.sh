09e7b08f13205c7dd1fc3186ab2ae5eb4b83f77c
Your Name <you@example.com>
2023-07-11 20:07:42 +0530

    updated to lil descriptive-part1

---
+++ 07-commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# There are 4 types of command available : 
+
+# 1) Binary                   ( /bin  , /sbin ) -----> Commands that come os level i.e cat,type
+# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# 3) Shell Built-in Commands  ls ; bash
+# 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
