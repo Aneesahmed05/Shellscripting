14156ff674d0c8b2f408fdaa72436da7e8464b6f
Your Name <you@example.com>
2023-07-11 18:11:23 +0530

    updated to lil descriptive-part1

--- 04-dynamic _variables.sh
+++ 04-dynamic _variables.sh
@@ -3,7 +3,9 @@
 #In scripting languages, dynamic values typically refer to data that can change or be modified during the execution of a program.
 #These values are not fixed or predetermined but can be assigned, updated, or manipulated based on various conditions or actions within the script.
 
+TODAYS_DATE=$(date +%F)                    
+NO_OF_SESSIONS=$(who | wc -l)                                                          # Always enclose expresssions in paranthesis
+echo -e "Good Morning and todays date is \e[32m $TODAYS_DATE \e[0m"
+echo -e "Number of opened sessions : \e[32m $NO_OF_SESSIONS \e[0m"
 
-TODAYS_DATE=$(date +%F)  
 
-TODAYS_DATE="03JUNE2023"
