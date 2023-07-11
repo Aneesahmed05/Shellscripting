84069d6a18b4ca546804b7d39933a77ebb690d4c
Your Name <you@example.com>
2023-07-11 19:59:52 +0530

    updated to lil descriptive-part1

--- 06-read.sh
+++ 06-read.sh
@@ -9,3 +9,12 @@ echo -e "Name of the entered value is $name"
 echo "Enter your name.."
 read name
 echo "Hello $name ! Learn to Read input from user in Bash Shell"
+
+
+#output
+[ centos@workstation ~/Shellscripting ]$ bash 06-read.sh 
+Enter your name :anees
+Name of the entered value is anees
+Enter your name..
+Ahmed
+Hello Ahmed ! Learn to Read input from user in Bash Shell
