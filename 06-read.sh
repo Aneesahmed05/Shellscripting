36c6a1a091b858df301368adfdda6b73966cb4af
Your Name <you@example.com>
2023-07-11 19:46:39 +0530

    updated to lil descriptive-part1

--- 06-read.sh
+++ 06-read.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
-read -p "Enter your name :" name 
-echo -e "Name of the entered value is $name"
-read -r "Enter your name :" name 
+#read -p "Enter your name :" name 
+#echo -e "Name of the entered value is $name"
+
+
+
+
+echo "Enter your name.."
+read name
+echo "Hello $name ! Learn to Read input from user in Bash Shell"
