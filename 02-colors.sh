de4dfc0711a8c9582f969af088b8fdd07af2ef6b
Your Name <you@example.com>
2023-07-11 16:28:20 +0530

    updated to lil descriptive-part1

---
+++ 02-colors.sh
@@ -0,0 +1,8 @@
+ # The syntax to print colors is 
+ # Ex:  
+ #      echo -e "\e[COL-CODEm  Your Message To Be Printed \e[0m"   ----->General format
+ #      echo -e "\e[32m I am printing Green Color \e[0m"
+
+
+
+echo -e "\e["32m Welcome to cloudtopia \e[0"
